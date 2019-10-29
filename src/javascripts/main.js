81083c7b99f3649a73677d56088d16f267cbbe1f
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-10-28 19:57:41 -0500

    added farmers

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import '../styles/main.scss';
 import cowList from './components/cowList/cowList';
+import farmersList from './components/farmersList/farmersList';
 
 import 'bootstrap';
 
 const init = () => {
   cowList.buildCows();
+  farmersList.buildFarmers();
 };
 init();
